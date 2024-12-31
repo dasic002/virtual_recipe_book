17de735e46f5cf94701426ab4fd54cb5b6a8966a
Carlos Silva <dasic002@yahoo.co.uk>
2024-12-31 11:08:19 +0000

    Update secret key

--- chefsnet/settings.py
+++ chefsnet/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3d46n-c+8tmw@$7$-lotw6_fimtti@n%!m8f4hyx*)xm5mqbxb'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- recipe_book/views.py
+++ recipe_book/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is your index page!")
+    return HttpResponse("This is your index page! As set in views.py")
