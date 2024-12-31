2407c5df32959d65e9cec73ee6f8e01cee49e8e6
Carlos Silva <dasic002@yahoo.co.uk>
2024-12-31 16:23:22 +0000

    Add Comment model

--- recipe_book/models.py
+++ recipe_book/models.py
@@ -47,3 +47,37 @@ class Recipe(models.Model):
     source_url = models.URLField(max_length=250)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
+class Comment(models.Model):
+    """
+    Stores a single Recipe comment entry related to 
+    :model:`recipe_book.Recipe`.
+    :model:`auth.User`.
+    """
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="commenter"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
