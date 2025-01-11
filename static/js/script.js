bfef76bee9d9c9b57f7a95c2ad6956817f7fcb9a
Carlos Silva <dasic002@yahoo.co.uk>
2025-01-11 13:29:53 +0000

    Update JS to dynamic add and remove ingredient forms
    Update ingredient formset to include can_delete
    
    Remove 'remove ingredient' button from template

--- static/js/script.js
+++ static/js/script.js
@@ -1,31 +1,88 @@
-let recipeForm = document.querySelectorAll(".multiField")
-let container = document.querySelector("#ingredient_form")
-let addButton = document.querySelector("#add-form")
-let removeButton = document.querySelectorAll(".remove-form")
-let totalForms = document.querySelector("#id_ingredients_needed-TOTAL_FORMS")
+let recipeForm = document.querySelectorAll(".multiField");
+let container = document.querySelector("#ingredients_form");
+let addBtn = document.querySelector("#add-form");
+let removeBtns = document.querySelectorAll(".remove-form");
+let totalForms = document.querySelector("#id_ingredients_needed-TOTAL_FORMS");
 
-let formNum = recipeForm.length - 1
-addButton.addEventListener('click', addForm)
+let formNum = recipeForm.length - 1;
+
+
+addBtn.addEventListener('click', addForm);
+
+container.onload = renderRemBtns();
+
 
 function addForm(e) {
-    e.preventDefault()
+    e.preventDefault();
     // copy form
-    let newForm = recipeForm[0].cloneNode(true)
-    let formRegex = RegExp(`ingredients_needed-(\\d){1}-`, 'g')
-    
-    // copy delete button
-    // let newDeleteBtn = removeButton[0].cloneNode()
-    // variable holding elements
-    // let formWithButton = newForm
-    // newForm = newForm.after(newDeleteBtn)
+    let newForm = recipeForm[0].cloneNode(true);
+    newForm.hidden = false;
 
-    formNum++
-    newForm.innerHTML = newForm.innerHTML.replace(formRegex, `ingredients_needed-${formNum}-`)
-    container.appendChild(newForm)
+    // setting the text to search for in the HTML
+    let formRegex = RegExp(`ingredients_needed-(\\d){1}-`, 'g');
 
-    // newDeleteBtn.innerHTML = newDeleteBtn.innerHTML.replace(formRegex, `ingredients_needed-${formNum}-`)
-    // container.appendChild(newDeleteBtn)
+    // generate form instance count
+    formNum++;
 
-    totalForms.setAttribute('value', `${formNum + 1}`)
-    console.log("Run the function!")
+    // add to fields values
+    newForm.innerHTML = newForm.innerHTML.replace(formRegex, `ingredients_needed-${formNum}-`);
+    container.appendChild(newForm);
+
+    // update the total forms value setting expected fields to accept
+    totalForms.setAttribute('value', `${formNum + 1}`);
+
+    // Update variable list
+    recipeForm = document.querySelectorAll(".multiField");
+
+    // update view of remove ingredient btns
+    renderRemBtns();
+    // console.log("Run the function!", e);
+}
+
+function removeForm(e) {
+    // get number of field and set hidden
+    console.log(e);
+    recipeForm[e].hidden = true;
+
+    // set delete checkbox to true
+    recipeForm[e].querySelectorAll('.checkboxinput')[0].checked = true;
+}
+
+
+// Function will run at onload and everytime an ingredient form is added
+// we need this at the start in case the form is refreshed or prepopulated from GET
+// the remove button is added per delete checkbox available in the form
+function renderRemBtns() {
+    let deleteChecks = container.querySelectorAll('.checkboxinput');
+
+    // template for the new remove ingredient button
+    const remBtn = document.createElement('button');
+    const btnText = document.createTextNode("Remove ingredient");
+    remBtn.appendChild(btnText);
+    remBtn.setAttribute('onclick', `removeForm(x)`);
+    remBtn.setAttribute('type', 'button');
+    remBtn.classList.add('remove-form');
+
+    // set the counter
+    let counter = 0;
+
+    // iterate through the delete checkboxes
+    for (let checkbox of deleteChecks) {
+        // find if given parent of checkbox contains any buttons like ours
+        let el = checkbox.parentElement.querySelectorAll('.remove-form');
+        // copy our template button to append
+        let newRemBtn = remBtn.cloneNode(true);
+
+        // if list el returned empty add our button
+        if (el.length == 0) {
+            checkbox.after(newRemBtn);
+            // on adding button, refresh list so we can setAttr below
+            el = checkbox.parentElement.querySelectorAll('.remove-form');
+        }
+
+        // setAttr with appropriate name
+        el[0].setAttribute('onclick', `removeForm(${counter})`);
+
+        counter++;
+    }
 }
